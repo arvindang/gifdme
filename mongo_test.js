2b4b408b0193f7bb36b1d5ae02ec7367b48ab256
Sean Muron <swmuron@gmail.com>
2013-03-06 03:32:07 -0600

    IM WIRED AS FUCK

--- mongo_test.js
+++ mongo_test.js
@@ -1,6 +1,12 @@
-static_tags = ["happy","sad"]
-tw = "I am currently very %happy"
-for (var i = 0, ii = static_tags.length; i < ii; i++) {
-	var re = new RegExp("[^\b]?[%]"+static_tags[i]+"\b?","gim");
-	if (re.test(tw) ) { console.log(static_tags[i]); }
-}
+var static_tags = ['happy','sad'];
+function tagFromTweet(tw) {
+	// Find tagged emotions in tweets
+	
+	for (var i = 0, ii = static_tags.length; i < ii; i++) {
+		var re = new RegExp("[^\b]?[%]"+static_tags[i]+"\b?","gim");
+		if (re.test(tw) ) { return static_tags[i]; }
+	}	
+	return "";
+};
+console.log(tagFromTweet("I am pretty happy tonight")!="");
+console.log(tagFromTweet("I am pretty %happy tonight")!="");
