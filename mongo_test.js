c5a13faa713082b64fe2fe098da5d92ed041ddc9
Sean Muron <swmuron@gmail.com>
2013-03-06 03:25:46 -0600

    FUCKING LOWERCASE E

--- mongo_test.js
+++ mongo_test.js
@@ -1,27 +1,6 @@
-// test mongoose
-var assert = require('assert');
-var mongo = require('mongodb');
-
- var client = new mongo.Db('test', new mongo.Server("127.0.0.1", 27017, {}), {w: 1}),
-        test = function (err, collection) {
-          collection.insert({a:2}, function(err, docs) {
-
-            collection.count(function(err, count) {
-              assert.ok(0<count);
-            });
-
-            // Locate all the entries using find
-            collection.find().toArray(function(err, results) {
-              assert.ok(0<results.length);
-              assert.ok(results[0].a === 2);
-
-              // Let's close the db
-              client.close();
-            });
-          });
-        };
-
-    client.open(function(err, p_client) {
-      client.collection('test_insert', test);
-      
-    });
+static_tags = ["happy","sad"]
+tw = "I am currently very %happy"
+for (var i = 0, ii = static_tags.length; i < ii; i++) {
+	var re = new RegExp("[^\b]?[%]"+static_tags[i]+"\b?","gim");
+	if (re.test(tw) ) { console.log(static_tags[i]); }
+}
